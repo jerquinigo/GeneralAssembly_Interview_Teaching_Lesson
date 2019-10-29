3f7a817f591ee45c02d10b0ecaf2efb3c0c37202
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-10-29 14:16:44 -0400

    adding lesson notes and classwork assignment

---
+++ excercise.js
@@ -0,0 +1,5 @@
+const myVariableName
+let biggestPartyEver!
+const even bigger party
+let smallest-party-ever
+const medium$sized$party 
---
+++ lessonNotes.js
@@ -0,0 +1,91 @@
+//data types
+// integers
+// strings
+// booleans
+// undefined and null
+
+// console.log(typeof(100),)
+//  console.log(typeof("hello world"))
+//  console.log(typeof(false))
+//  console.log(typeof(true))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let and const 
+
+//  let favLanguage = "javascript"
+//  console.log(favLanguage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// camelCase
+// snake_case
+
+
+
+// good variable names
+//letters
+//numbers
+//underscores
+//dollar signs ($)
+
+//variable names cannot contain
+
+//spaces
+//dashes
+//periods
+// capital first letters starting variable names
+
+
+
+// GOOD EXAMPLES 
+let firstName = "Jonathan"
+
+
+ let favoriteLanguage = "JavaScript"
+ let favorite2ndLanguage = "python3"
+
+
+
+//BAD EXAMPLES
+//let first-name = "jonathan"
+
+// using dashes will throw an error
+
+//let my favorite color = "blue"
+// having spaces will throw an error
+
+//let my.js.string.variable = "this is my variable"
+// having periods to seperate the words will throw errors
+
+//let Myname = "jonathan"
+// console.log(Myname)
+
+// let newArray = new Array()
+// console.log(newArray)
+
+// JavaScript will accept this, but is very bad practice because javascript classes use capitals. 
